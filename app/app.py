a908d2ee5b3bca7660f0f4f675658494f3d5101c
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-04-01 13:33:08 +0300

    moved sql queries into their own queries.py file

--- app/app.py
+++ app/app.py
@@ -10,6 +10,8 @@ from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
+import queries
+
 app = Flask(__name__)
 load_dotenv()
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
@@ -32,9 +34,9 @@ def index():
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    sql = "SELECT id, password FROM users WHERE name=:username"
-    result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()    
+   
+    user = queries.getUser(username)
+
     if not user:
         redirect("/")
     else:
@@ -63,24 +65,18 @@ def addaccount():
     password = request.form["password"]
     adress = request.form["adress"]
     phonenumber = request.form["phonenumber"]
-    if username != '' and password != '' and adress != '' and phonenumber != '':
-        sql = "SELECT COUNT(id) FROM users WHERE name=:username"
-        result = db.session.execute(sql, {"username":username})
-        usersWithSameName = result.fetchone()    
-        if usersWithSameName[0] > 0:
-            return redirect("/")
-
-
-        hash_value = generate_password_hash(password)
-        sql = "INSERT INTO users (name, password, adress, phonenumber) VALUES (:username, :password, :adress, :phonenumber)"
-        db.session.execute(sql, {"username":username, "password":hash_value, "adress":adress, "phonenumber":phonenumber})
-        db.session.commit()
-    return redirect("/")
+   
+    hash_value = generate_password_hash(password)
+   
+    #TODO: error handling
+    if queries.addUserToDataBase(username, hash_value, adress, phonenumber):
+        return redirect("/")
+    else:
+        return redirect("/")
 
 @app.route("/manageBooks")
 def manageBooks():
-    result = db.session.execute("SELECT * FROM books")
-    books = result.fetchall()
+    books = queries.getAllBooks()
     return render_template("manageBooks.html", count=len(books), books=books)
 
 #help from: https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/ for handling file uploads
@@ -90,7 +86,6 @@ def addBook():
     publishDate = request.form["publishDate"]
     amountFree = request.form["amountFree"]
     amountOverAll = request.form["amountOverall"]
-    
     content = request.files['content']        
     filename = content.filename
     pathToFile = ''
@@ -99,71 +94,57 @@ def addBook():
         pathToFile = os.path.join(app.config['UPLOAD_FOLDER'], filenameToWrite)
         content.save(pathToFile)
         
-
-
-    if name != '' and publishDate != '' and amountFree != '' and amountOverAll != '' and pathToFile != '':
-        sql = "INSERT INTO books (name, publishDate, amount_free, amount_all, book_path) VALUES (:name, :publishDate, :amountFree, :amountOverAll, :pathToFile)"
-        db.session.execute(sql, {"name":name, "publishDate":publishDate, "amountFree":amountFree, "amountOverAll":amountOverAll, "pathToFile": pathToFile})
-        db.session.commit()
-    return redirect("/manageBooks")
+    #TODO: error handling
+    if queries.insertBook(name, publishDate, amountFree, amountOverAll, pathToFile):
+        return redirect("/manageBooks")
+    else:
+        return redirect("/manageBooks")
+   
+  
+   
 
 @app.route("/borrowBooks")
 def borrowBooks():
-    result = db.session.execute("SELECT * FROM books")
-    books = result.fetchall()
+    books = queries.getAllBooks()
     return render_template("borrowBooks.html", books=books)
 
 @app.route("/borrowBooks/borrow", methods=["POST"])
 def borrow():
    
     bookId = request.form["book-id"]
-    if bookId == '':
-         return redirect("/borrowBooks")
-    sql = "SELECT COUNT(id) FROM borrows WHERE user_id=:userId AND book_id=:bookId"
-    result = db.session.execute(sql, {"userId":session["user_id"], "bookId":bookId})
-    maara = result.fetchone()
-    if maara[0] > 0:
+     #TODO: error handling
+    if queries.borrowBook(bookId):
+       return redirect("/borrowBooks")
+    else:
         return redirect("/borrowBooks")
-    queryToUpdateBorrows = "INSERT INTO borrows (user_id, book_id) VALUES (:userId, :bookId)"
-    queryToUpdateBookAmounts = "UPDATE books SET amount_free = amount_free - 1 WHERE id=:bookId"
-    db.session.execute(queryToUpdateBorrows, {"userId":session["user_id"], "bookId":bookId})
-    db.session.execute(queryToUpdateBookAmounts, {"bookId":bookId})
-    db.session.commit()
-    return redirect("/borrowBooks")
 
 @app.route("/borrowinformation/")
 def borrowinformation():
-    sql = "SELECT books.id, name, publishdate FROM borrows JOIN books ON borrows.book_id = books.id;"
-    result = db.session.execute(sql, {"userId":session["user_id"]})
-    books = result.fetchall()
+
+    books = queries.getBooksOfUser()
+    
     return render_template("borrowinformation.html", books=books)
 
 @app.route("/borrowinformation/returnBook", methods=["POST"])
 def returnBook():
     bookId = request.form["book-id"]
-    if bookId == '':
-         return redirect("/borrowBooks")
-    
-    queryToUpdateBorrows = "DELETE FROM borrows WHERE user_id=:userId AND book_id=:bookId"
-    queryToUpdateBookAmounts = "UPDATE books SET amount_free = amount_free + 1 WHERE id=:bookId"
-    db.session.execute(queryToUpdateBorrows, {"userId":session["user_id"], "bookId":bookId})
-    db.session.execute(queryToUpdateBookAmounts, {"bookId":bookId})
-    db.session.commit()
-    
-    return redirect("/borrowinformation/")
+    #TODO: error handling
+    if queries.returnBook(bookId):
+        return redirect("/borrowinformation/")
+    else:
+        return redirect("/borrowInformation")
 
 @app.route("/readBook", methods=["POST"])
 def readBook():
     bookId = request.form["book-id"]
-    if bookId == '':
-        return redirect("/bookinformation")
-    sql = "SELECT books.name, book_path FROM borrows JOIN books ON books.id = borrows.book_id WHERE borrows.user_id =:userId AND borrows.book_id =:bookId"
-    result = db.session.execute(sql, {"userId":session["user_id"], "bookId":bookId})
-    bookData = result.fetchone()
-
-    bookFile = open(bookData.book_path, "r")
-    bookContent = []
-    for rivi in bookFile:
-        bookContent.append(rivi)
-    
-    return render_template("readbook.html", bookName=bookData.name, bookContent=bookContent)
+   
+    bookData = queries.getBookReadingData(bookId)
+    #TODO: error handling
+    if bookData:
+        bookFile = open(bookData.book_path, "r")
+        bookContent = []
+        for rivi in bookFile:
+            bookContent.append(rivi)
+        return render_template("readbook.html", bookName=bookData.name, bookContent=bookContent)
+    else:
+        return render_template("/borrowInformation")
---
+++ app/queries.py
@@ -0,0 +1,86 @@
+from dotenv import load_dotenv
+from flask import Flask
+from flask import session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+
+
+
+from werkzeug.security import check_password_hash, generate_password_hash
+app = Flask(__name__)
+load_dotenv()
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
+
+def addUserToDataBase(username, hash_value, adress, phonenumber):
+     if username != '' and hash_value != '' and adress != '' and phonenumber != '':
+        sql = "SELECT COUNT(id) FROM users WHERE name=:username"
+        result = db.session.execute(sql, {"username":username})
+        usersWithSameName = result.fetchone()    
+        if usersWithSameName[0] > 0:
+            return False
+
+        sql = "INSERT INTO users (name, password, adress, phonenumber) VALUES (:username, :password, :adress, :phonenumber)"
+        db.session.execute(sql, {"username":username, "password":hash_value, "adress":adress, "phonenumber":phonenumber})
+        db.session.commit()
+        return True
+
+def getUser(username):
+    sql = "SELECT id, password FROM users WHERE name=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    return user    
+
+def getAllBooks():
+     result = db.session.execute("SELECT * FROM books")
+     books = result.fetchall()
+     return books
+
+def insertBook(name, publishDate, amountFree, amountOverAll, pathToFile):
+    if name != '' and publishDate != '' and amountFree != '' and amountOverAll != '' and pathToFile != '':
+        sql = "INSERT INTO books (name, publishDate, amount_free, amount_all, book_path) VALUES (:name, :publishDate, :amountFree, :amountOverAll, :pathToFile)"
+        db.session.execute(sql, {"name":name, "publishDate":publishDate, "amountFree":amountFree, "amountOverAll":amountOverAll, "pathToFile": pathToFile})
+        db.session.commit()
+
+def borrowBook(bookId):
+    if bookId == '':
+         return False
+  
+    sql = "SELECT COUNT(id) FROM borrows WHERE user_id=:userId AND book_id=:bookId"
+    result = db.session.execute(sql, {"userId":session["user_id"], "bookId":bookId})
+    maara = result.fetchone()
+    if maara[0] > 0:
+        return False
+    queryToUpdateBorrows = "INSERT INTO borrows (user_id, book_id) VALUES (:userId, :bookId)"
+    queryToUpdateBookAmounts = "UPDATE books SET amount_free = amount_free - 1 WHERE id=:bookId"
+    db.session.execute(queryToUpdateBorrows, {"userId":session["user_id"], "bookId":bookId})
+    db.session.execute(queryToUpdateBookAmounts, {"bookId":bookId})
+    db.session.commit()
+    return True
+
+def getBooksOfUser():
+    sql = "SELECT books.id, name, publishdate FROM borrows JOIN books ON borrows.book_id = books.id WHERE borrows.user_id=:userId;"
+    result = db.session.execute(sql, {"userId":session["user_id"]})
+    books = result.fetchall()
+    return books
+
+def returnBook(bookId):
+    if bookId == '':
+         return False
+    
+    queryToUpdateBorrows = "DELETE FROM borrows WHERE user_id=:userId AND book_id=:bookId"
+    queryToUpdateBookAmounts = "UPDATE books SET amount_free = amount_free + 1 WHERE id=:bookId"
+    db.session.execute(queryToUpdateBorrows, {"userId":session["user_id"], "bookId":bookId})
+    db.session.execute(queryToUpdateBookAmounts, {"bookId":bookId})
+    db.session.commit()
+    return True
+
+def getBookReadingData(bookId):
+    if bookId == '':
+        return None
+    sql = "SELECT books.name, book_path FROM borrows JOIN books ON books.id = borrows.book_id WHERE borrows.user_id =:userId AND borrows.book_id =:bookId"
+    result = db.session.execute(sql, {"userId":session["user_id"], "bookId":bookId})
+    bookData = result.fetchone()
+    return bookData
